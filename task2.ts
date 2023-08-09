067eb94a2a9f0c8c14b77909cf70991dd123552f
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-08-09 23:33:02 +0500

    added task2 and export variable from task1

--- task2.ts
+++ task2.ts
@@ -1,11 +1,15 @@
 let arr = [1,3,-4,7,8,-9,11,12,-14,13,16,15,17,-18,20];
 //console.log(arr);
 let even = [];
-console.log("even Numbers are")
+let odd = [];
+console.log("Odd Numbers are")
 for (let i = 0; i<arr.length; i++){
     if (arr[i]%2==0){
         even[i]= arr[i]
-        console.log(even[i]);
+        //console.log(even[i]);
+    }
+    else {
+        odd[i]=arr[i];
+        console.log(odd[i])
     }
 }
-export default arr;
