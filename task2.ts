d5a941d95a8365190555608c55a3b74adc0f0994
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-08-11 20:06:05 +0500

    update 3 files

--- task2.ts
+++ task2.ts
@@ -1,15 +1,15 @@
-import arr from './task1'
+let arr2 = [1,3,-4,7,8,-9,11,12,-14,13,16,15,17,-18,20];
 //console.log(arr);
-let even = [];
+let even2 = [];
 let odd = [];
 console.log("Odd Numbers are")
-for (let i = 0; i<arr.length; i++){
-    if (arr[i]%2==0){
-        even[i]= arr[i]
+for (let i = 0; i<arr2.length; i++){
+    if (arr2[i]%2==0){
+        even2[i]= arr2[i]
         //console.log(even[i]);
     }
     else {
-        odd[i]=arr[i];
+        odd[i]=arr2[i];
         console.log(odd[i])
     }
 }
